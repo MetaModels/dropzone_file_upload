f64dba4d16f7bdcd7625fe7b1676cb7cb278e3ff
Sven Baumann <baumann.sv@gmail.com>
2022-11-21 03:06:27 +0100

    Fixed author validation issue

--- src/Hook/InitialIzeDropzoneUpload.php
+++ src/Hook/InitialIzeDropzoneUpload.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/dropzone_file_upload.
  *
- * (c) 2019 The MetaModels team.
+ * (c) 2019-2022 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -12,7 +12,8 @@
  *
  * @package    MetaModels/attribute_file
  * @author     Sven Baumann <baumann.sv@gmail.com>
- * @copyright  2019 The MetaModels team.
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2019-2022 The MetaModels team.
  * @license    https://github.com/MetaModels/dropzone_file_upload/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
---
+++ tests/DependencyInjection/MetaModelsDropzoneFileUploadExtensionTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace MetaModels\DropzoneFileUploadBundle\Test\DependencyInjection;
+
+use MetaModels\DropzoneFileUploadBundle\DependencyInjection\MetaModelsDropzoneFileUploadExtension;
+use PHPUnit\Framework\TestCase;
+
+class MetaModelsDropzoneFileUploadExtensionTest extends TestCase
+{
+
+}
