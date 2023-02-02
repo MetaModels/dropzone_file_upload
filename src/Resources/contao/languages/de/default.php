fa90af216289f15fbfa6ed5ebcfed3aac071ad5f
Mini Model <minimodel@metamodel.me>
2023-02-02 08:15:56 +0100

    Updated translations from transifex.

---
+++ src/Resources/contao/languages/de/default.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This file is part of MetaModels/dropzone_file_upload.
+ *
+ * (c) 2012-2023 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * Translations are managed automatically using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * Last-updated: 2023-02-02T07:15:51+00:00
+ *
+ * @copyright 2012-2023 The MetaModels team.
+ * @license   https://github.com/MetaModels/dropzone_file_upload/blob/master/LICENSE LGPL-3.0-or-later
+ * @link      https://www.transifex.com/metamodels/public/
+ * @link      https://www.transifex.com/signup/?join_project=metamodels
+ */
+
+
+$GLOBALS['TL_LANG']['ERR']['maxFileUpload'] = 'Der Upload ist auf %s Datei(en) beschränkt.';
+
---
+++ src/Resources/contao/languages/de/tl_metamodel_dcasetting.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * This file is part of MetaModels/dropzone_file_upload.
+ *
+ * (c) 2012-2023 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * Translations are managed automatically using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * Last-updated: 2023-02-02T07:15:55+00:00
+ *
+ * @copyright 2012-2023 The MetaModels team.
+ * @license   https://github.com/MetaModels/dropzone_file_upload/blob/master/LICENSE LGPL-3.0-or-later
+ * @link      https://www.transifex.com/metamodels/public/
+ * @link      https://www.transifex.com/signup/?join_project=metamodels
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['dropzone_settings_legend']                 = 'Dropzone-Einstellungen';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['fe_widget_file_dropzone']['0']             = 'Dropzone';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['fe_widget_file_dropzone']['1']             = 'Falls aktiviert wird im Widget ein anpassbares Dropzone-Feld benutzt.';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['fe_widget_file_dropzone_description']['0'] = 'Dropzone-Beschreibung';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['fe_widget_file_dropzone_description']['1'] = 'Hier können Sie eine Beschreibung für die Dropzone festlegen.';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['fe_widget_file_dropzone_hide_label']['0']  = 'Label nach dem Upload verbergen';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['fe_widget_file_dropzone_hide_label']['1']  = 'Wenn diese Option aktiviert ist wird das Label verborgen, nachdem ein Upload erfolgt ist.';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['fe_widget_file_dropzone_label']['0']       = 'Dropzone-Label';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['fe_widget_file_dropzone_label']['1']       = 'Hier können Sie das Label für Dropzone festlegen. Das Label wird im Dropzone-Feld angezeigt werden.';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['fe_widget_file_dropzone_limit']['0']       = 'Dropzone-Beschränkung';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['fe_widget_file_dropzone_limit']['1']       = 'Hier können Sie festlegen, wie viele Dateien maximal hochgeladen werden können. Falls leer können unbegrenzt viele Dateien hochgeladen werden.';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['fe_widget_file_dropzone_removable']['0']   = 'Entfernen möglich';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['fe_widget_file_dropzone_removable']['1']   = 'Wenn diese Option aktiviert ist können eingefügte Dateien aus der Dropzone entfernt werden.';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['fe_widget_file_dropzone_template']['0']    = 'Angepasstes Template für die Nutzung von Dropzone';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['fe_widget_file_dropzone_template']['1']    = 'Wählen Sie das Template aus, das für Dropzone benutzt werden soll. Ein gültiger Dateiname für das Template beginnt mit "mm_form_field_dropzone".';
+
